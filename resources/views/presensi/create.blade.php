18967a62d7d21021ba0442881a6ee0fea955d592
avrians <avriansyahb10@gmail.com>
2023-09-02 08:02:23 +0700

    [Av][05][Done]

--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -1,16 +1,5 @@
  @extends('layouts.presensi')
 
- <style>
-     .webcam-capture, 
-     .webcam-capture video{
-        display: inline-block;
-         width: 100% !important;
-         margin: auto;
-         height: auto !important;
-         border-radius: 15px;
-     }
- </style>
-
  @section('header')
      <div class="appHeader bg-primary text-light">
          <div class="left">
@@ -22,12 +11,32 @@
          <div class="right"></div>
      </div>
  @endsection
+
+ <style>
+     .webcam-capture,
+     .webcam-capture video {
+         display: inline-block;
+         width: 100% !important;
+         margin: auto;
+         height: auto !important;
+         border-radius: 15px;
+     }
+ </style>
+
  @section('content')
      <div class="row" style="margin-top: 70px">
          <div class="col">
+             <input type="hidden" id="lokasi">
              <div class="webcam-capture">
 
              </div>
+         </div>
+     </div>
+     <div class="row">
+         <div class="col">
+             <button id="takeabsen" class="btn btn-primary btn-block mt-1">
+                 <ion-icon name="camera-outline"></ion-icon>
+                 Absen Masuk</button>
          </div>
      </div>
  @endsection
@@ -43,5 +52,17 @@
              });
              Webcam.attach('.webcam-capture');
          });
+         var lokasi = document.getElementById('lokasi');
+         if (navigator.geolocation) {
+             navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+         }
+
+         function successCallback(position) {
+             lokasi.value = position.coords.latitude + "," + position.coords.longitude;
+         }
+
+         function errorCallback(position) {
+
+         }
      </script>
  @endpush
